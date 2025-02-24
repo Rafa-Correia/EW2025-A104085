50b3a351b40c5ccfa0b08dc0037a8516c4519d0d
Rafa-Correia <rafaelazevedocorreia04@gmail.com>
2025-02-24 15:46:37 +0000

    AulaT_4

---
+++ AulaT_4/script.py
@@ -0,0 +1,13 @@
+#fills the dataset with ids 
+
+import json
+
+f = open("treinos_dataset.json", encoding='utf-8')
+bd = json.load(f)
+f.close
+
+for i, reg in enumerate(bd["treinos"]):
+    reg["id"] = "t" + str(i)
+
+f = open("treinos_dataset.json", "w", encoding='utf-8')
+json.dump(bd, f, ensure_ascii=False)
