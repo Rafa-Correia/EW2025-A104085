e92df50e91b7fd2f654bcde257462dfac56c166a
Rafa-Correia <rafaelazevedocorreia04@gmail.com>
2025-03-10 15:48:25 +0000

    AulaT_5 theory complete

--- AulaT_5/cinema/routes/index.js
+++ AulaT_5/cinema/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+    { 
+      title: 'EngWeb2025', 
+      docente: 'JCR',
+      instituicao: 'DI-UM'
+    });
 });
+
+router.get('/movies', function(req, res, next) {
+  axios.get('http://localhost:3000/movies')
+    .then(resp => {
+      movieList = resp.data
+      res.render('movies', {movieList: resp.data, tit: 'Movie List'})
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error', {error: err})
+    }) 
+});
+
+
 
 module.exports = router;
